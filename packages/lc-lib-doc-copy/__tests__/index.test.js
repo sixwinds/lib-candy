2729c04c9f02380c0c2dfa0add0cbed558b37720
Bernie Wang <bernie.wangbj@gmail.com>
2020-01-13 11:05:05 +0800

    feat: add package.json template of doc-copy

--- packages/lc-lib-doc-copy/__tests__/index.test.js
+++ packages/lc-lib-doc-copy/__tests__/index.test.js
@@ -6,7 +6,8 @@ const basePath = __dirname;
 const dist = {
   readmeFile: path.resolve(basePath, './README.md'),
   changelogFile: path.resolve(basePath, './CHANGELOG.md'),
-  docDir: path.resolve(basePath, './doc')
+  docDir: path.resolve(basePath, './doc'),
+  packageFile: path.resolve(basePath, './package.json')
 }
 
 describe('lc-lib-doc-copy', function() {
@@ -14,17 +15,26 @@ describe('lc-lib-doc-copy', function() {
     fse.removeSync(dist.readmeFile);
     fse.removeSync(dist.changelogFile);
     fse.removeSync(dist.docDir);
+    fse.removeSync(dist.packageFile);
   });
   afterEach(function() {
     fse.removeSync(dist.readmeFile);
     fse.removeSync(dist.changelogFile);
     fse.removeSync(dist.docDir);
+    fse.removeSync(dist.packageFile);
   });
   test('copy without data', function() {
     lcLibDocCopy({
-      basePath: basePath
+      basePath: basePath,
+      libraryName: 'jslib',
+      libraryVersion: '0.1.0'
     })
-    const content = fse.pathExistsSync(dist.readmeFile)
-    expect(content).toBe(true)
+    const success = 
+      fse.pathExistsSync(dist.readmeFile) &&
+      fse.pathExistsSync(dist.changelogFile) &&
+      fse.pathExistsSync(dist.docDir) &&
+      fse.pathExistsSync(dist.packageFile);
+    
+    expect(success).toBe(true)
   });
 });
--- packages/lc-lib-doc-copy/index.js
+++ packages/lc-lib-doc-copy/index.js
@@ -4,26 +4,35 @@ const copyTemplate = require('lc-util/copyTemplate');
 /* 
   @param {Object} [options]
   @param {string} options.basePath - doc dist path
-  @param {string} options.libName
+  @param {string} options.libraryName
 */
 function lcLibDocCopy(options) {
   options = options || {};
   const basePath = options.basePath || process.cwd();
-  const libName = options.libName;
+  const libraryName = options.libraryName;
+  const libraryVersion = options.libraryVersion;
 
   copyTemplate(
     path.resolve(__dirname, './template/README.md'),
     path.resolve(basePath, './README.md'),
-    { libraryName: libName }
+    { libraryName: libraryName }
   );
   copyTemplate(
     path.resolve(__dirname, './template/doc/api.md'),
     path.resolve(basePath, './doc/api.md'),
-    { libraryName: libName }
+    { libraryName: libraryName }
   );
   copyTemplate(
     path.resolve(__dirname, './template/CHANGELOG.md'),
     path.resolve(basePath, './CHANGELOG.md')
+  );
+  copyTemplate(
+    path.resolve(__dirname, './template/package.json'),
+    path.resolve(basePath, './package.json'),
+    {
+      libraryName: libraryName,
+      libraryVersion: libraryVersion
+    }
   );
 }
 
