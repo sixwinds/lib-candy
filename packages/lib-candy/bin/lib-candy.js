0bbfb5183a647e1be3eef9363f21608883116e31
Bernie Wang <bernie.wangbj@gmail.com>
2020-01-13 17:37:49 +0800

    feat: add lib-candy code

--- packages/lib-candy/bin/lib-candy.js
+++ packages/lib-candy/bin/lib-candy.js
@@ -1,3 +1,32 @@
 #!/usr/bin/env node
+const path = require('path');
+const spawn = require('cross-spawn');
 const libDocCopy = require('lc-lib-doc-copy');
 
+const devDependencies = [
+  'lc-lib-rollup-build',
+  'lc-jest-test'
+];
+const libName = process.argv[2];
+const libRootDir = path.resolve(libName);
+
+libDocCopy({
+  basePath: libRootDir,
+  libraryName: libName
+});
+
+process.chdir(libRootDir);
+const installArgs = [
+  'install',
+  '--save-dev',
+  '--loglevel',
+  'error'
+].concat(devDependencies);
+const command = 'npm'
+const child = spawn(command, installArgs, { stdio: 'inherit' });
+
+child.on('close', function(code, signal) {
+  if (code !== 0) {
+    console.error('error: ' + code + ' ' + signal)
+  }
+});
