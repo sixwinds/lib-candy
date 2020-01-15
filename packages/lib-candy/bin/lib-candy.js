b0730dabd1e2365b129f9c6b703bd9b8c01b54f6
Bernie Wang <bernie.wangbj@gmail.com>
2020-01-15 15:09:21 +0800

    feat: add processing tips

--- packages/lib-candy/bin/lib-candy.js
+++ packages/lib-candy/bin/lib-candy.js
@@ -10,6 +10,7 @@ const devDependencies = [
 ];
 const command = process.argv[2];
 const libName = process.argv[3];
+const libPath = path.resolve(libName);
 
 function printUsage() {
   console.log();
@@ -21,32 +22,50 @@ function printUsage() {
   console.log();
 }
 
+function printSuccessTips() {
+  console.log();
+  console.log(`Success! Created ${libName} at ${libPath}`);
+  console.log('Inside that directory, you can run several commands:');
+  console.log();
+  console.log(' npm run build');
+  console.log('   Bundles the library into static files for production.');
+  console.log();
+  console.log(' npm test');
+  console.log('   Starts the test runner.');
+  console.log();
+  console.log('Happy coding!');
+}
+
 if (command === 'new' && libName) {
-  const libRootDir = path.resolve(libName);
-  
+  // copy template
   libDocCopy({
-    basePath: libRootDir,
+    basePath: libPath,
     libraryName: libName
   });
   
-  process.chdir(libRootDir);
+  // cd to project dir and install npm packages
+  process.chdir(libPath);
   const installArgs = [
     'install',
     '--save-dev',
     '--loglevel',
     'error'
   ].concat(devDependencies);
-  const command = 'npm'
+  const command = 'npm';
+  console.log('Installing packages. This might take a couple of minutes.');
+  console.log();
   const child = spawn(command, installArgs, { stdio: 'inherit' });
   
   child.on('close', function(code, signal) {
     if (code !== 0) {
       console.log();
+      console.log('Aborting installation.')
       console.log('Failed to run command: ' + command + ' ' + installArgs.join(' '))
       console.log('error: ' + code);
       console.log('signal: ' + signal)
       console.log()
     } else {
+// set npm scripts command
       const pkgJsonStr = fs.readFileSync('./package.json', 'utf8');
       const pkgJson = JSON.parse(pkgJsonStr);
       if (!pkgJson.scripts) {
@@ -55,6 +74,7 @@ if (command === 'new' && libName) {
       pkgJson.scripts.test = 'lc-jest-test';
       pkgJson.scripts.build = 'lc-lib-rollup-build';
       fs.writeFileSync('./package.json', JSON.stringify(pkgJson, null, '  '));
+      printSuccessTips();
     }
   });
 } else {
