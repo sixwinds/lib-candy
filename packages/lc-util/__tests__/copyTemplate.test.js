e52d258e0331b4748fd211e18f58ae91b9ff63d1
Bernie Wang <bernie.wangbj@gmail.com>
2020-01-15 16:24:33 +0800

    feat(lc-util): add option overwritten of copyTemplate

--- packages/lc-util/__tests__/copyTemplate.test.js
+++ packages/lc-util/__tests__/copyTemplate.test.js
@@ -5,6 +5,7 @@ const copyFile = require('../copyTemplate');
 
 const srcFilePath = path.resolve(__dirname, './template/example.md');
 const distFilePath = path.resolve(__dirname, './projRoot/example.md');
+const existedFilePath = path.resolve(__dirname, './projRoot/existedExample.md');
 
 describe('copyTemplate', function() {
   beforeEach(function() {
@@ -13,11 +14,21 @@ describe('copyTemplate', function() {
   afterEach(function() {
     fse.removeSync(distFilePath);
   });
-  test('copy without data', function() {
+  test('copy with data', function() {
     copyFile(srcFilePath, distFilePath, {
-      libraryName: undefined
+      data: {
+        libraryName: 'my-third-lib'
+      }
     });
     const content = fs.readFileSync(distFilePath, 'utf8');
-    expect(content).toBe('This is an example of template: .')
+    expect(content).toBe('This is an example of template: my-third-lib.')
+  });
+
+  test('not to be overwritten', function() {
+    copyFile(srcFilePath, existedFilePath, {
+      overwritten: false
+    });
+    const content = fs.readFileSync(existedFilePath, 'utf8');
+    expect(content).toBe('existed example')
   });
 });
--- packages/lc-util/copyTemplate.js
+++ packages/lc-util/copyTemplate.js
@@ -2,8 +2,20 @@ const fs = require('fs');
 const fse = require('fs-extra');
 const ejs = require('ejs');
 
-function copyTemplate(from, to, data) {
-  data = data || {};
+/* 
+  @param {string} from - template source path
+  @param {string} to - dest path
+  @param {Object} [options] - options
+  @param {Object} [options.data={}] - template date
+  @param {string} [options.overwritten=true] - wether is dest file to be overwrittem if it exists 
+*/
+function copyTemplate(from, to, options) {
+  options = options || {};
+  data = options.data || {};
+
+  if (options.overwritten === false && fse.pathExistsSync(to)) {
+    return;
+  }
   const tmpl = fs.readFileSync(from, 'utf8');
   const distContent = ejs.render(tmpl, data);
   fse.outputFileSync(to, distContent);
